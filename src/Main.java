3f783732d66a7aa455b199768dd17ca4ef747f2d
Greddyy <55572482+Greddyy@users.noreply.github.com>
2020-05-06 22:55:44 +0300

    Added exceptions when string contains ' ' and '-' symbols

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,33 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-    public static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter your word");
         String input = sc.nextLine();
         System.out.println(isogram(input));
 
+    }
+    // write your code here
 
-}
-	// write your code here
-
-   public static boolean isogram(String str) {
+    public static boolean isogram(String str) {
         boolean iso = true;
         str = str.toLowerCase();
         int len = str.length();
         char[] arr = str.toCharArray();
         Arrays.sort(arr);
         for (int i = 0; i < len - 1; i++) {
-            if (arr[i] == arr[i + 1]) {
-                return iso = false;
-            }
+            if (arr[i] == ' ' || arr[i] == '-')
+                continue;
+            for (int j = i + 1; j<i; j++){
+                if (arr[i]==arr[j])
+                    return false;
 
+            }
         }
-        return iso;
+        return true;
     }
 }
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("Enter your word");
-//        String input = sc.nextLine();
-//        char[] chars = input.toCharArray();
-//        for (int i = 0; i <= input.length() - 1; i++) {
-//            if (chars[i] == chars[i + 1])
-//                return false;
-//        }
-//        return true;
+
+
